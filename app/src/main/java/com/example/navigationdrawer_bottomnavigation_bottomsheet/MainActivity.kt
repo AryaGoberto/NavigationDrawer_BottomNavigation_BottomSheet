edafad28be662b156b34b81924e88623d3414d47
AryaGoberto <arguna2019@gmail.com>
2025-04-23 11:24:23 +0800

    Modifikasi bottom nav dan bottom sheet

--- app/src/main/java/com/example/navigationdrawer_bottomnavigation_bottomsheet/MainActivity.kt
+++ app/src/main/java/com/example/navigationdrawer_bottomnavigation_bottomsheet/MainActivity.kt
@@ -1,27 +1,44 @@
 package com.example.navigationdrawer_bottomnavigation_bottomsheet
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
 import androidx.compose.material.icons.filled.ExitToApp
+import androidx.compose.material.icons.filled.Favorite
 import androidx.compose.material.icons.filled.Home
+import androidx.compose.material.icons.filled.Notifications
 import androidx.compose.material.icons.filled.Person
+import androidx.compose.material.icons.filled.PlayArrow
+import androidx.compose.material.icons.filled.Search
 import androidx.compose.material.icons.filled.Settings
+import androidx.compose.material.icons.filled.Star
+import androidx.compose.material.icons.filled.ThumbUp
 import androidx.compose.material.icons.rounded.Menu
+import androidx.compose.material3.BottomAppBar
 import androidx.compose.material3.Divider
 import androidx.compose.material3.DrawerValue
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.ModalBottomSheet
 import androidx.compose.material3.ModalDrawerSheet
 import androidx.compose.material3.ModalNavigationDrawer
 import androidx.compose.material3.NavigationDrawerItem
@@ -38,10 +55,17 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
 import com.example.navigationdrawer_bottomnavigation_bottomsheet.ui.theme.Blacky
 import com.example.navigationdrawer_bottomnavigation_bottomsheet.ui.theme.NavigationDrawer_BottomNavigation_BottomSheetTheme
@@ -65,6 +89,7 @@ class MainActivity : ComponentActivity() {
     }
 }
 
+@SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun LearnNavBotSheet() {
@@ -77,7 +102,7 @@ fun LearnNavBotSheet() {
         mutableStateOf(Icons.Default.Home)
     }
     val sheetState = rememberModalBottomSheetState()
-    val showBottomSheet by remember {
+    var showBottomSheet by remember {
         mutableStateOf(false)
     }
     ModalNavigationDrawer(
@@ -86,9 +111,9 @@ fun LearnNavBotSheet() {
         drawerContent = {
             ModalDrawerSheet {
                 Box(modifier = Modifier
-                    .fillMaxSize()
+                    .fillMaxWidth()
                     .background(Blacky)
-                    .height(150.dp)
+                    .height(100.dp)
                 ){
                     Text(text = "")
                 }
@@ -171,10 +196,122 @@ fun LearnNavBotSheet() {
                         }
                     }
                 )
+            },
+            bottomBar = {
+                BottomAppBar (containerColor = Blacky){
+                    IconButton(onClick = {
+                        selected.value = Icons.Default.Home
+                        navigationController.navigate(Screens.Home.screens){
+                            popUpTo(0)
+                        }
+                    }, modifier = Modifier.weight(1f)) {
+                        Icon(
+                            Icons.Default.Home,
+                            contentDescription = null,
+                            modifier = Modifier.size(26.dp),
+                            tint = if(selected.value ==Icons.Default.Home) Color.White else Color.DarkGray)
+                    }
+                    IconButton(onClick = {
+                        selected.value = Icons.Default.Search
+                        navigationController.navigate(Screens.Search.screens){
+                            popUpTo(0)
+                        }
+                    }, modifier = Modifier.weight(1f)) {
+                        Icon(
+                            Icons.Default.Search,
+                            contentDescription = null,
+                            modifier = Modifier.size(26.dp),
+                            tint = if(selected.value ==Icons.Default.Search) Color.White else Color.DarkGray)
+                    }
+                    Box(modifier = Modifier
+                        .weight(1f)
+                        .padding(16.dp),
+                        contentAlignment = Alignment.Center){
+                        FloatingActionButton(onClick = {showBottomSheet= true}) {
+                            Icon(Icons.Default.Add, contentDescription = null, tint = Oranye)
+
+                        }
+                    }
+                    IconButton(onClick = {
+                        selected.value = Icons.Default.Notifications
+                        navigationController.navigate(Screens.Notification.screens){
+                            popUpTo(0)
+                        }
+                    }, modifier = Modifier.weight(1f)) {
+                        Icon(
+                            Icons.Default.Notifications,
+                            contentDescription = null,
+                            modifier = Modifier.size(26.dp),
+                            tint = if(selected.value ==Icons.Default.Notifications) Color.White else Color.DarkGray)
+                    }
+                    IconButton(onClick = {
+                        selected.value = Icons.Default.Person
+                        navigationController.navigate(Screens.Profile.screens){
+                            popUpTo(0)
+                        }
+                    }, modifier = Modifier.weight(1f)) {
+                        Icon(
+                            Icons.Default.Person,
+                            contentDescription = null,
+                            modifier = Modifier.size(26.dp),
+                            tint = if(selected.value ==Icons.Default.Person) Color.White else Color.DarkGray)
+                    }
+                }
             }
         ){
-            //BottomBar
+            NavHost(navController = navigationController,
+                startDestination = Screens.Home.screens){
+                composable(Screens.Home.screens) { Home() }
+                composable(Screens.Profile.screens) { Profile() }
+                composable(Screens.Notification.screens) { Notification() }
+                composable(Screens.Post.screens) { Post() }
+                composable(Screens.Settings.screens) { Settings() }
+
+            }
+        }
+        if(showBottomSheet){
+            ModalBottomSheet(onDismissRequest = {showBottomSheet = false}, sheetState = sheetState) {
+                Column(modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(18.dp),
+                    verticalArrangement = Arrangement.spacedBy(20.dp)){
+                    BottomSheetItem(icon = Icons.Default.ThumbUp, title = "Create a post") {
+                        showBottomSheet = false
+                        navigationController.navigate(Screens.Post.screens){
+                            popUpTo(0)
+                        }
+                    }
+                    BottomSheetItem(icon = Icons.Default.Star, title = "Add a Story") {
+                       Toast.makeText(context, "Story", Toast.LENGTH_SHORT).show()
+                    }
+                    BottomSheetItem(icon = Icons.Default.PlayArrow, title = "Add a Story") {
+                        Toast.makeText(context, "Reels", Toast.LENGTH_SHORT).show()
+                    }
+                    BottomSheetItem(icon = Icons.Default.Favorite, title = "Add a Story") {
+                        Toast.makeText(context, "Live", Toast.LENGTH_SHORT).show()
+                    }
+                }
+            }
         }
     }
+}
+
+@Composable
+fun BottomSheetItem(icon:ImageVector, title:String, onClick:()->Unit) {
+    Row(
+        verticalAlignment = Alignment.CenterVertically,
+        horizontalArrangement = Arrangement.spacedBy(12.dp),
+        modifier = Modifier.clickable { onClick() }
+    ){
+        Icon(icon, contentDescription = null, tint = Oranye)
+        Text(text = title, color = Oranye, fontSize = 22.sp )
+    }
+    
+}
+
+@Composable
+@Preview
+fun LearnNavBotSheetPreview(){
+    LearnNavBotSheet()
 }
 
